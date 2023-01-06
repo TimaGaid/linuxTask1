0f0b98c634d529624a301c5295c79371efb5084b
Тимофій Гайдамаченко <timofijgaidamacenko@MacBook-Pro-Timofij.local>
2023-01-06 12:54:08 +0200

    last change

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -2,3 +2,8 @@ variable "region" {
   default = "us-east-1"
   type    = string
 }
+
+variable "pub_key" {
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCik7pMAQ2w0EhZGtBn9BmIvBi+C8wn1Zu0/gV+CG4qSZEdNwS9pQZ/c1u+Wxt4cpdnfSWidz4TJc+NUTPNzmFLcmvKLq9ol/Z7LaovKuWRunrLOqwuUUUr+cw9Oo3nOcNEEmHzez/wPalJ4IK6RHuqFJ+r4gkC8crlj9QHgF/b2RGI6tq2JzGQ+wylvcTK5HRN1i6TAF7SHKDWLvj9Lo9VRWEUHON4fc96dVnsyd4aqtMymp3h3buni0NsVQPZwBXViOuwbyc8a9Ipx4W2o2/j0YM3AJCpeDANZoj69BGQNANvNLt+Q+GKzfwk7ziXYZssed2/fWvKRzka30eBw0X1sJDduSuBH1pbXe1Jb0vGyUGWh6MLEzfJdGT78tAg/0UaiGC4KcBo1MwNz9ZWf8KDudPkN/V8QRX5+HhmSz94Ba6LSO8oIwbl37Z5CKU5zktdHvRKoa0IuIAlgcQT0yrHWwI3oje83WcPMUOWEzRdNQEfOS6OztFjn0VGcXQolmk= timofijgaidamacenko@MacBook-Pro-Timofij.local"
+  type = string
+}
